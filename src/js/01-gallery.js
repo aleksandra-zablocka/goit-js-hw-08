dae7b69470ae76e15710bc1f6a46bba3bf9bf269
Aleksandra Zabłocka <aleksandra-zablocka@o2.pl>
2023-05-03 11:56:04 +0200

    finished task 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,47 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+console.log(SimpleLightbox);
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+
+createGalleryItems();
+
+function createGalleryItems() {
+  for (const galleryItem of galleryItems) {
+    const galleryLi = document.createElement('li');
+    galleryLi.classList.add('gallery__li');
+    gallery.append(galleryLi);
+
+    const galleryLink = document.createElement('a');
+    galleryLink.classList.add('gallery__item');
+    galleryLink.href = galleryItem.original;
+    galleryLi.append(galleryLink);
+
+    const galleryImage = document.createElement('img');
+    galleryImage.classList.add('gallery__image');
+    galleryImage.src = galleryItem.preview;
+    galleryImage.alt = galleryItem.description;
+    galleryImage.dataset.source = galleryItem.original;
+    galleryLink.append(galleryImage);
+  }
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  scrollZoom: false,
+});
+
+const openImage = event => {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+};
+
+gallery.addEventListener('click', openImage);
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const updateTime = throttle(
+  pausedTime =>
+    localStorage.setItem('videoplayer-current-time', pausedTime.seconds),
+  1000
+);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+player.on('timeupdate', updateTime);
