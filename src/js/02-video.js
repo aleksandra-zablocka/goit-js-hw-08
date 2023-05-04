ebb81a4d5fb7b54ac9f8f2000c1d172862bc619d
Aleksandra Zabłocka <aleksandra-zablocka@o2.pl>
2023-05-04 10:33:58 +0200

    'done tasks 2 and 3'

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,12 +4,19 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const updateTime = throttle(
-  pausedTime =>
-    localStorage.setItem('videoplayer-current-time', pausedTime.seconds),
-  1000
-);
+// const updateTime = throttle(
+//   pausedTime =>
+//     localStorage.setItem('videoplayer-current-time', pausedTime.seconds),
+//   1000
+// );
+
+// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+// player.on('timeupdate', updateTime);
+
+const updateTime = pausedTime =>
+  localStorage.setItem('videoplayer-current-time', pausedTime.seconds);
 
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 
-player.on('timeupdate', updateTime);
+player.on('timeupdate', throttle(updateTime, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,52 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+// function saveToLocalStorage() {
+//   try {
+//     const feedback = {
+//       email: emailInput.value,
+//       message: messageInput.value,
+//     };
+//     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(feedback));
+//   } catch (error) {
+//     console.error('Error saving to localStorage:', error);
+//   }
+// }
+
+const saveToLocalStorage = throttle(e => {
+  try {
+    const feedback = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(feedback));
+  } catch (error) {
+    console.error('Error saving to localStorage:', error);
+  }
+}, 500);
+
+window.addEventListener('load', () => {
+  try {
+    const previousValue = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    if (previousValue) {
+      emailInput.value = previousValue.email;
+      messageInput.value = previousValue.message;
+    }
+  } catch (error) {
+    console.error('Error reading from localStorage:', error);
+  }
+});
+
+const sumbitForm = event => {
+  event.preventDefault();
+  emailInput.value = '';
+  messageInput.value = '';
+  //   localStorage.clear();
+};
+
+form.addEventListener('input', saveToLocalStorage);
+form.addEventListener('submit', sumbitForm);
